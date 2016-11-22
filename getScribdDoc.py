b7d23dc7bf45012bc946eac3b935fd853192a3bb
Sudi <sbhowmik@email.com>
2016-11-22 10:00:19 -0700

    Update getScribdDoc.py

--- getScribdDoc.py
+++ getScribdDoc.py
@@ -2,23 +2,19 @@ __author__ = 'sbhowmik'
 '''-----------------------------------------------------------------------
 This gets all the jpgs that constitutes the whole document. One needs to
  download the source for the document by right clicking and saving source as a html
- This downloaded htl=ml forms the input for this SCRIBD downloader
  --------------------------------------------------------------------------'''
 # Download Utility for files
 
 def mydownload(url, fileName=None):
     import urllib2
     import shutil
-    import urlparse
-    import os
-    #r = urllib2.urlopen(urllib2.Request(url))
+
     try:
         r = urllib2.urlopen(urllib2.Request(url))
     except:
         print 'Could not download ' + url
         return
     try: #Copy file
-        fileName = fileName or getFileName(url,r)
         with open(fileName, 'wb') as f:
             shutil.copyfileobj(r,f)
     except:
@@ -26,11 +22,10 @@ def mydownload(url, fileName=None):
     finally:
         r.close()
 
-from pyPdf import PdfFileReader,PdfFileWriter
+
 import os.path
 htmlSource = r'D:\Personal\test\Written\Python\Scribd_scrape\dwld_dir\Carson-W-Taylor-Power-System-Voltage-Stability-1994.htm'
 foundId = False
-pdfObj = PdfFileWriter()
 
 with open(htmlSource,'r') as fd: # read the saved html file
     pgcount = 1
@@ -51,21 +46,7 @@ with open(htmlSource,'r') as fd: # read the saved html file
                     mydownload(url,opImg) #str(pgcount)+'.jpg'
                 else:
                     print 'exists skipping ' +opImg
-#                im = Image.open(str(pgcount)+'.pdf')
-#                dpi = im.size
-#                im.save(str(pgcount)+'.pdf','PDF')
-#                pdf = PdfFileReader(file(str(pgcount)+'.pdf',"rb"))
-#                #for page in pdf.pages:
-#                pdfObj.addPage(pdf.getPage(0))
-                #os.remove('tmp.pdf')
                 pgcount = pgcount +1
 
-    print "Pdf has %s pages." % pdfObj.getNumPages()
-    outStream = file(htmlSource[:-3]+'pdf', 'wb')
-    pdfObj.write(outStream)
-    outStream.close()
 if not foundId:
     print "WARNING!! Could not find ID. nothing done"
-
-
-
